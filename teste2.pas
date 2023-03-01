f7d5941f04435ee4a1088968303e7f86d2674028
jefesonk1 <jefesonk1@outlook.com>
2023-03-01 02:37:09 -0300

    -

---
+++ teste2.pas
@@ -0,0 +1,20 @@
+' + - * / = < > [ ] . , ( ) : ^ @ { } $22 # & / + - + %11 22.0
+<< >> ** <> >< <= >= := += -= *= /= (* *) (. .) //
+(* My beautiful function returns an interesting result *)
+{ My beautiful function returns an interesting result }
+{ My beautiful function returns an interesting result }
+{ My beautiful function returns an interesting result }
+(* This is an old style comment *)
+{  This is a Turbo Pascal comment }
+// This is a Delphi comment. All is ignored till the end of the line.
+{ Comment 1 (* comment 2 *) }
+(* Comment 1 { comment 2 } *)
+{ comment 1 // Comment 2 }
+(* comment 1 // Comment 2 *)
+// comment 1 (* comment 2 *)
+// comment 1 { comment 2 }
+'teste'
+'ttt'
+''
+' '
+'"'
